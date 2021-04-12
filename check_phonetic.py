6180f51a8acc524b932cdb317c9dfae82609feb5
David Larson <dlarson314@users.noreply.github.com>
2021-04-11 23:05:38 -0400

    In progress

--- check_phonetic.py
+++ check_phonetic.py
@@ -1,3 +1,4 @@
+import collections
 import re
 
 import main as cmu
@@ -109,12 +110,90 @@ def foo4():
   for i in range(100):
     print(pairs[i])
 
+def group_word(word, vowels={'a', 'e', 'i', 'o', 'u'}):
+  endIndices = []
+  vowels = {v.lower() for v in vowels}
+  for i in range(len(word) - 1):
+    isVowel0 = (word[i].lower() in vowels)
+    isVowel1 = (word[i+1].lower() in vowels)
+    if isVowel0 != isVowel1:
+      endIndices.append(i+1)
+  startIndices = [0] + endIndices
+  endIndices.append(len(word))
+  #print(startIndices)
+  #print(endIndices)
+  return [word[start:end] for start, end in zip(startIndices, endIndices)]
+
+def foo5():
+  print(group_word('scrabble'))
+  print(group_word('supercalafragilisticexpialidocious'))
+
+  word = re.sub('\d', '', 'EH0 M B AA1 R K').split()
+  print(word)
+  print(group_word(word, vowels={'EH', 'AA'}))
+
+def foo6():
+  cmulist = cmu.load_cmudict()
+  cmudict = {word.upper(): (sounds, ipa) for word, sounds, ipa in cmulist}
+
+  found = []
+  missing = []
+  with open('2of12.txt') as f:
+    for line in f:
+      word = line.strip().upper()
+      if word not in cmudict:
+        missing.append(word)
+      else:
+        found.append(word)
+  print(len(missing), len(found))
+
+  matched = []
+
+  hist = collections.Counter()
+
+  with open('2of12_found.txt', 'w') as f:
+    for word in found:
+      sounds, ipa = cmudict[word]
+      f.write('%s\t%s\t/%s/\n' % (word,  ' '.join(sounds), ''.join(ipa)))
+
+      word2 = re.sub('\W', '', word)
+      groups0 = group_word(word2)
+      sounds = [re.sub('\d', '', sound) for sound in sounds]
+      groups1 = group_word(sounds, vowels=vowels)
+      if len(groups0) != len(groups1):
+        #print(word, groups0, groups1)
+        pass
+      else:
+        matched.append(word)
+        for english, sound in zip(groups0, groups1):
+          hist[(english, tuple(sound))] += 1
+  first = {key[0] for key in hist.keys()}
+  second = {key[1] for key in hist.keys()}
+  print('number of english groups: ', len(first))
+  print('number of sound groups: ' , len(second))
+  print('number of pairs: ' , len(hist))
+
+  to_ipa = cmu.load_key()
+
+  with open('sound_hist.txt', 'w') as f:
+    pairs = list(hist.keys())
+    pairs.sort()
+    for word, sounds in pairs:
+      ipa = [to_ipa[re.sub('\d', '', s)] for s in sounds]
+      f.write('%5d\t%s\t/%s/\t%s\n' % (hist[(word, sounds)], word, ''.join(ipa), sounds))
+
+  print(len(matched))
+
+
+
 
 
 if __name__ == "__main__":
   #foo()
   #foo2()
   #foo3()
-  foo4()
+  #foo4()
+  #foo5()
+  foo6()
 
 
